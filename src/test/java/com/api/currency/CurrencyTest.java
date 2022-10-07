7ce5897eedb3fc048d47f9e9937bd9b98cb81e45
lisa <lise.hickey@gmail.com>
2022-10-07 11:28:45 +0100

    refactored test class to use a requestSpec builder instead of hardcoding uri in each test method

--- src/test/java/com/api/currency/CurrencyTest.java
+++ src/test/java/com/api/currency/CurrencyTest.java
@@ -1,15 +1,30 @@
 package com.api.currency;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 
 public class CurrencyTest {
 
+    private static RequestSpecification requestSpec;
+
+    @BeforeAll
+    public static void createRequestSpec(){
+        requestSpec = new RequestSpecBuilder().
+                setBaseUri("https://api.coindesk.com/v1/bpi").
+                build();
+    }
+
     @Test
     public void priceOfBitcoinIsReturnedSuccessfully() {
-        when().get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        given().spec(requestSpec).
+        when().get("/currentprice.json")
                 .then()
                 .statusCode(HttpStatus.SC_OK);
 
